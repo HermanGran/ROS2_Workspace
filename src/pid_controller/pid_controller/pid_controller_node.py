d920d6e98d2aaed9f8ac03e7e6068732e17ad94e
matswj <matswiikjensen@gmail.com>
2025-03-31 18:03:12 +0200

    La til ROS2 service-server for å sette reference

--- src/pid_controller/pid_controller/pid_controller_node.py
+++ src/pid_controller/pid_controller/pid_controller_node.py
@@ -2,6 +2,8 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
 from rcl_interfaces.msg import SetParametersResult
+from pid_controller_msgs.srv import SetReference  # <- Manglet
+import math
 
 class PIDControllerNode(Node):
     def __init__(self):
@@ -19,9 +21,13 @@ class PIDControllerNode(Node):
         self.reference = self.get_parameter('reference').value
 
         self.add_on_set_parameters_callback(self.parameter_callback)
+
         self.voltage = 0.0
         self.previous_error = 0.0
         self.integral = 0.0
+
+        # Service-server
+        self.srv = self.create_service(SetReference, 'set_reference', self.set_reference_callback)
 
         # ROS2 Publisher og Subscriber
         self.publisher_ = self.create_publisher(Float64, 'voltage', 10)
@@ -61,7 +67,18 @@ class PIDControllerNode(Node):
             elif param.name == 'reference' and param.value >= 0.0:
                 self.reference = param.value
                 self.get_logger().info(f'Reference updated to: {self.reference}')    
-        return SetParametersResult(successful=True)    
+        return SetParametersResult(successful=True)
+
+    def set_reference_callback(self, request, response):
+        value = request.request
+        if -math.pi <= value <= math.pi:
+            self.reference = value
+            self.get_logger().info(f"Reference updated via service to: {value:.2f}")
+            response.success = True
+        else:
+            self.get_logger().warn(f"Invalid reference value: {value:.2f} (outside [-π, π])")
+            response.success = False
+        return response
 
 def main(args=None):
     rclpy.init(args=args)
@@ -72,3 +89,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
