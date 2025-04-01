fd4bda4ba5a11a3fc1472a4e7c74d2f7e37b221f
hermangran <herman.h.gran@hotmail.com>
2025-04-01 13:25:43 +0200

    Finished part B

--- src/pid_controller/launch/launch.py
+++ src/pid_controller/launch/launch.py
@@ -1,25 +1,27 @@
+import os
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from ament_index_python.packages import get_package_share_directory
+
+
 
 def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('pid_controller'),
+        'config',
+        'parameters.yaml'
+        )
+
     return LaunchDescription([
-        DeclareLaunchArgument('kp', default_value='1.0'),
-        DeclareLaunchArgument('ki', default_value='0.0'),
-        DeclareLaunchArgument('kd', default_value='0.0'),
-    
-    
         Node(
             package='pid_controller',
             executable='pid_controller_node',
             name='pid_controller_node',
             output='screen',
-            parameters=[{
-                'p': LaunchConfiguration('kp'),
-                'i': LaunchConfiguration('ki'),
-                'd': LaunchConfiguration('kd'),
-            }]
+            parameters=[config]
         ),
         Node(
             package='joint_simulator',
