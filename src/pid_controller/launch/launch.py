f448b80c05bdba7b7df8094a3d29de6bf4f09462
matswj <matswiikjensen@gmail.com>
2025-04-01 11:23:02 +0200

    Add launch.py and update setup.py to include launch file

---
+++ src/pid_controller/launch/launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='pid_controller',
+            executable='pid_controller_node',
+            name='pid_controller_node',
+            output='screen'
+        ),
+        Node(
+            package='joint_simulator',
+            executable='joint_simulator',
+            name='joint_simulator',
+            output='screen'
+        ),
+        Node(
+            package='reference_input',
+            executable='reference_input_node',
+            name='reference_input_node',
+            output='screen'
+        )
+    ])
