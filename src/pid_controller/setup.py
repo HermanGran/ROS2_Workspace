e77ef2b5ac467ca56edd752ca614c1ccf344e627
hermangran <herman.h.gran@hotmail.com>
2025-04-15 15:19:47 +0200

    Added paramerters.yaml to setup.py

--- src/pid_controller/setup.py
+++ src/pid_controller/setup.py
@@ -1,4 +1,7 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
+from setuptools import setup
 
 package_name = 'pid_controller'
 
@@ -10,6 +13,7 @@ setup(
     ('share/ament_index/resource_index/packages',
         ['resource/' + package_name]),
     ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/config', ['config/parameters.yaml']),
     ('share/' + package_name + '/launch', ['launch/launch.py']),
 ],
     install_requires=['setuptools'],
