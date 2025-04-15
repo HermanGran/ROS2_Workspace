30d5b701c1ab2297f9cfe1c5e0098250ab1bd53e
matswj <matswiikjensen@gmail.com>
2025-04-15 16:47:33 +0200

    changed link name to map

--- src/joint_description/launch/view_model.launch.py
+++ src/joint_description/launch/view_model.launch.py
@@ -1,19 +1,17 @@
 import os
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
 from ament_index_python.packages import get_package_share_directory
 
-
 def generate_launch_description():
-
-    robot_description_content = os.path.join(
+    urdf_path = os.path.join(
         get_package_share_directory('joint_description'),
         'urdf',
         'joint_model.urdf'
     )
+
+    with open(urdf_path, 'r') as infp:
+        robot_description_content = infp.read()
 
     return LaunchDescription([
         Node(
@@ -26,7 +24,6 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            output='screen',
-            arguments=[]
+            output='screen'
         )
     ])
